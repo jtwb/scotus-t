02dc88fd82cff66a609082c341d82377ad22c090
Jason Benterou <jason.benterou+github@gmail.com>
2015-06-21 20:35:20 -0700

    Scripts

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM python:2-onbuild
 
 VOLUME /var/dataset
 
-CMD [ "python", "./__main__.py" ]
+CMD [ "scrapy", "runspider", "spiders/oyez.py", "-o", "/var/dataset/crawl.json" ]
---
+++ run_bash.sh
@@ -0,0 +1,7 @@
+docker run \
+    --name scrapy0 \
+    -ti --rm \
+    -p 8889:8888 \
+    -v /Users/jason/src/scotus-t/data:/var/dataset \
+    -e "PASSWORD=test28" \
+    jtwb/scotus-t /bin/bash
